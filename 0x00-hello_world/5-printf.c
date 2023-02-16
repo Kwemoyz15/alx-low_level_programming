6f630210b8548fc0a6e28a28227fe59e3bd6f0e3
Kwemoyz15 <kevinkwemoyz@gmail.com>
2023-02-16 12:12:24 -0800

    5-printf.c-1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  * Description: This function prints a message to the console using the printf() function.
- * Return: Always 0 (Success)
+ * Return: Always 0 
  */
 
 int main(void)
