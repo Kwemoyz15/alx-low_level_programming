a7a964ef5bc68a26c2775f753c555ea87759fba0
Kwemoyz15 <kevinkwemoyz@gmail.com>
2023-02-27 04:42:46 -0800

    Function that sets the value of an integer to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ * reset_to_98 - sets the value of an integer to 98
+ *
+ * @n: pointer to the integer to be set to 98
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+		printf("n=%d\n", *n);
+}
