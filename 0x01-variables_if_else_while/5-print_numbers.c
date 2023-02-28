88bf18ae56d964575cdeb5e740b3fffdb86f8886
Kwemoyz15 <kevinkwemoyz@gmail.com>
2023-02-28 12:35:47 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,12 +9,11 @@
 int main(void)
 {
 	int i;
-	char j;
 
-	for (i = 0 ; i < 10 ; i++)
-		putchar(i + '0');
-	for (j = 'a' ; j <= 'f' ; j++)
-		putchar(j);
-	putchar('\n');
+	for (i = 0 ; i < 10; i++)
+	{
+		printf("%d", i);
+		printf("\n");
+	}
 	return (0);
 }
