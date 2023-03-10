4b2cdd738f96c741508877b165070101cf02900a
Kwemoyz15 <kevinkwemoyz@gmail.com>
2023-03-10 05:04:10 -0800

    libmy.a, main.h

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+void _puts(char *str)
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: This function prints a message to the console using the
+ * puts() function.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
