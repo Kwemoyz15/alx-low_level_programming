8460b28382362798f545d2658c96aea70240230c
Kwemoyz15 <kevinkwemoyz@gmail.com>
2023-03-10 07:51:46 -0800

    create_static_lib.sh

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,12 +1,14 @@
-#include "main.h"
-void _puts(char *str)
-#include<stdio.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
+ *
  * Description: This function prints a message to the console using the
- * puts() function.
+ *              puts() function.
+ *
  * Return: Always 0 (Success)
- */
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
