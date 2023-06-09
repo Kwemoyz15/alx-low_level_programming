99baa25f598e3663c5d13fc482b23194e1ddefdc
Kwemoyz15 <kevinkwemoyz@gmail.com>
2023-06-09 09:49:46 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - program that counts all arguments
+ * @argc: counts arguments
+ * @argv: array of arguments
+ */
+
+int main( int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void)argv;
+	return (0);
+}
